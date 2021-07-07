2b0183263450c051b6a89e41f309fab1a1d8b90f
zhiyuan <18336915720@163.com>
2021-07-07 14:53:53 +0800

    初始化项目结构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        主页面视图
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    created() {
+
+    },
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+    computed: {
+
+    },
+};
 </script>
+
+<style scoped lang="less">
+
+
+
+</style>
